4e40704ae2117c5698098ab2aa375c84ad6f2e05
MKS-Student <mks.student@hackreactor.com>
2016-09-16 20:05:09 -0700

    collisions are working

--- client/watchout.js
+++ client/watchout.js
@@ -2,8 +2,8 @@
 var ball = {};
 ball.id = 1;
 ball.r = 30;
-ball.cx = Math.random()*(window.innerWidth-2*ball.r);
-ball.cy = Math.random()*(window.innerHeight-2*ball.r);
+ball.cx =window.innerWidth*0.45;
+ball.cy =window.innerHeight*0.45;
 
 // Create new position for 20 enemy balls
 var numberOfEnemy = 20;
@@ -22,6 +22,18 @@ d3.select('.board').append('svg').attr('class', 'svgBoard'); //delete board(dont
 // .style('background-color',)
 var d3svgBoard = d3.select('.svgBoard');
 
+
+
+// Create enemy ball
+var d3enemyBall = d3svgBoard.selectAll('div')
+.data(createNewBall())
+.enter()
+.append('circle')
+.attr('cx', function(d){return d[0]})
+.attr('cy', function(d){return d[1]})
+.attr('r', radius)
+.attr('class','enemyBall')
+.attr('fill', 'blue');
 // Create player ball
 d3svgBoard.selectAll('circle')
 .data([ball], function(d){return d.id})
@@ -30,18 +42,8 @@ d3svgBoard.selectAll('circle')
 .attr('cx', function(d){return d.cx}) //shorten it
 .attr('cy', function(d){return d.cy})
 .attr('r', function(d){return d.r})
-.attr('class','playerBall');
-
-// Create enemy ball
-d3svgBoard.selectAll('div')
-.data(createNewBall())
-.enter()
-.append('circle')
-.attr('cx', function(d){return d[0]})
-.attr('cy', function(d){return d[1]})
-.attr('r', radius)
-.attr('class','enemyBall');
-// Update enemy ball
+.attr('class','playerBall')
+.attr('fill', 'red');
 
 
 d3svgBoard.on('mousemove', function(){
@@ -50,3 +52,42 @@ d3svgBoard.on('mousemove', function(){
   .attr('cx', position[0])
   .attr('cy', position[1]);
 });
+var speed = 2000;
+setInterval(function() {
+  d3enemyBall.data(createNewBall()).transition().duration(speed)
+  .attr('cx', function(d){return d[0]})
+  .attr('cy', function(d){return d[1]})
+}, speed);
+var collisionCount = 0;
+var playerCanCollide = true;
+var checkCollision = function(){
+  var currentEnemyPos = [];
+  var enemyBalls = document.getElementsByClassName('enemyBall');
+
+  _.each(enemyBalls, function(enemy){
+    currentEnemyPos.push([enemy.getAttribute('cx'), enemy.getAttribute('cy')]);
+  })
+  var currentPlayer = document.getElementsByClassName('playerBall')[0];
+  var currentPlayerPos = [currentPlayer.getAttribute('cx'), currentPlayer.getAttribute('cy')];
+  for (var i = 0;i < currentEnemyPos.length;i++){
+    var d = Math.sqrt(Math.pow(currentEnemyPos[i][0]-currentPlayerPos[0],2)+Math.pow(currentEnemyPos[i][1]-currentPlayerPos[1],2));
+    if (d < radius*2 && playerCanCollide){
+      collisionCount++;
+      playerCanCollide = false;
+      setTimeout(function(){
+        playerCanCollide = true;
+      }, 3000);
+      d3.select('.collisions')
+      .text('Collisions: ' + collisionCount);
+      d3svgBoard.transition().duration(200).style('background-color', 'blue');
+    }
+  }
+}
+
+// Collision Test
+setInterval(function(){checkCollision()}, 100);
+setInterval(function(){
+  d3svgBoard.transition().duration(1500).style('background-color', 'white')}, 3000);
+setInterval(function(){
+
+})
